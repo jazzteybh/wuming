ef9623a5033cf9383004a99d88a2af1be65af8aa
jazzteybh <jazzteybh@gmail.com>
2026-06-13 17:44:27 +0800

    Reduce life path to single digit 1-9 for Taiwan market

--- src/lib/numerology.ts
+++ src/lib/numerology.ts
@@ -1,7 +1,7 @@
 export function calculateLifePath(birthDate: string): number {
   const digits = birthDate.replace(/-/g, '').split('').map(Number)
   let sum = digits.reduce((a, b) => a + b, 0)
-  while (sum > 9 && sum !== 11 && sum !== 22 && sum !== 33) {
+  while (sum > 9) {
     sum = sum.toString().split('').map(Number).reduce((a, b) => a + b, 0)
   }
   return sum
